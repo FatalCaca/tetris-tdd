98c9d84c9f11f6468a78cd12de634f695953f2b3
Simon Balluais <simon.ballu@gmail.com>
2014-07-06 00:13:25 +0200

    Add folders

---
+++ android/src/com/tetris/tdd/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.tetris.tdd.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.tetris.tdd.TetrisTdd;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TetrisTdd(), config);
+	}
+}
